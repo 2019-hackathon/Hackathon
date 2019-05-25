09a1172d12a0f2aafee1898bbe5713f82bae07f9
Daniel Min <danieldmin216@gmail.com>
2019-05-25 17:10:52 -0400

    fixed drawer and readme

--- drawer.py
+++ drawer.py
@@ -4,7 +4,7 @@ import PIL
 import pyscreenshot as ImageGrab
 
 class main:
-    def __init__(self, master):
+    def __init__(self,master):
         self.master = master
         self.color_fg = 'black'
         self.color_bg = 'white'
@@ -15,26 +15,29 @@ class main:
         self.c.bind('<B1-Motion>',self.paint)
         self.c.bind('<ButtonRelease-1>',self.reset)
 
-    def paint(self, e):
+    def paint(self,e):
         if self.old_x and self.old_y:
-            self.c.create_line(self.old_x,self.old_y,e.x, e.y, width=self.penwidth, fill=self.color_fg, capstyle=ROUND, smooth=True)
+            self.c.create_line(self.old_x,self.old_y,e.x,e.y,width=self.penwidth,fill=self.color_fg,capstyle=ROUND,smooth=True)
 
         self.old_x = e.x
         self.old_y = e.y
 
-    def reset(self, e):
+    def reset(self,e):
         self.old_x = None
         self.old_y = None
 
+    def changeW(self,e):
+        self.penwidth = e
+
     def save(self):
-        file = filedialog.asksaveasfilename(filetypes=[('Portable Network Graphics', '*.png')])
+        file = filedialog.asksaveasfilename(filetypes=[('Portable Network Graphics','*.png')])
         if file:
             x = self.master.winfo_rootx() + self.c.winfo_x()
             y = self.master.winfo_rooty() + self.c.winfo_y()
             x1 = x + self.c.winfo_width()
-            y1 = self.c.winfo_height()
+            y1 = y + self.c.winfo_height()
 
-            ImageGrab.grab().crop((x, y, x1, y1)).save(file + '.png')
+            ImageGrab.grab().crop((x,y,x1,y1)).save(file)
 
     def clear(self):
         self.c.delete(ALL)
@@ -46,36 +49,31 @@ class main:
         self.color_bg=colorchooser.askcolor(color=self.color_bg)[1]
         self.c['bg'] = self.color_bg
 
-    def changeW(self, e):
-        self.penwidth = e
-
     def drawWidgets(self):
-        self.controls = Frame(self.master, padx=5, pady=5)
-        Label(self.controls, text='Pen Width: ', font=('', 15)).grid(row=0, column=0)
-        self.slider = ttk.Scale(self.controls, from_=5, to=100, command=self.changeW, orient=HORIZONTAL)
+        self.controls = Frame(self.master,padx = 5,pady = 5)
+        Label(self.controls, text='Pen Width: ',font=('',15)).grid(row=0,column=0)
+        self.slider = ttk.Scale(self.controls,from_= 5, to = 100, command=self.changeW,orient=HORIZONTAL)
         self.slider.set(self.penwidth)
         self.slider.grid(row=0,column=1,ipadx=30)
         self.controls.pack()
 
-        self.c = Canvas(self.master, width=500, height=400, bg=self.color_bg)
-        self.c.pack(fill=BOTH, expand=True)
+        self.c = Canvas(self.master,width=500,height=400,bg=self.color_bg,)
+        self.c.pack(fill=BOTH,expand=True)
 
         menu = Menu(self.master)
         self.master.config(menu=menu)
-
         filemenu = Menu(menu)
         menu.add_cascade(label='File..',menu=filemenu)
         filemenu.add_command(label='Export..',command=self.save)
-
+        colormenu = Menu(menu)
+        menu.add_cascade(label='Colors',menu=colormenu)
+        colormenu.add_command(label='Brush Color',command=self.change_fg)
+        colormenu.add_command(label='Background Color',command=self.change_bg)
         optionmenu = Menu(menu)
         menu.add_cascade(label='Options',menu=optionmenu)
         optionmenu.add_command(label='Clear Canvas',command=self.clear)
         optionmenu.add_command(label='Exit',command=self.master.destroy)
 
-        colormenu = Menu(menu)
-        menu.add_cascade(label='Colors', menu=colormenu)
-        colormenu.add_command(label='Brush Color', command=self.change_fg)
-        colormenu.add_command(label='Background Color', command=self.change_bg)
 
 
 if __name__ == '__main__':
