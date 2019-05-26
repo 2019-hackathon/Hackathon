a4e22a3bfd31842b461ef847d8e2b5d7e9c91cf1
Daniel Min <danieldmin216@gmail.com>
2019-05-25 21:56:45 -0400

    change of plans

--- livree_editor.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from tkinter import ttk, colorchooser, filedialog
-from tkinter.messagebox import *
-from tkinter.filedialog import *
-
-import PIL
-import pyscreenshot as ImageGrab
-import os
