f59e5e030990073352dccda29232a964e283ddde
Daniel Min <danieldmin216@gmail.com>
2019-05-26 01:19:55 -0400

    the real deal.

--- mealdeal.py
+++ mealdeal.py
@@ -25,8 +25,50 @@ print('Pick a meal:')
 for n, meal in enumerate(real_meals):
     print(f"{n+1}. {meal}")
 
-meal = real_meals[int(input('> ')) - 1]
+number = int(input('> '))
+meal = real_meals[number - 1]
+actual_meal = meal
 print(f'Here is the information for: {meal}')
 
+meal = food.split('},{')[number - 1]
+
+lst = meal.split(',')
+img = re.search('("strMealThumb":")(.*)"', lst[1])
+image_link = re.sub(r'\\', '', img.group(2))
+
+p = re.compile(r'\d+')
+for match in p.finditer(lst[2]):
+    food_id = match.group(0)
+
 # Download image for food
-print(food)
+with open('food.jpg', 'wb') as handle:
+        response = requests.get(image_link, stream=True)
+        for block in response.iter_content(1024):
+            if not block:
+                break
+            handle.write(block)
+
+recipe = requests.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772').text
+p = re.compile(r'"strInstructions":"(.+)","strMealThumb"')
+recette = re.finditer(p, recipe)
+
+recipe = ''
+for i in recette:
+    recipe = i.group(0)[19:-16]
+
+recipe = re.sub(r'\\r\\n', '<br>', recipe)
+
+# Things we actually care about
+# actual_meal - name of meal
+# recipe - recipe
+
+with open('food_info.html', 'w') as f:
+    f.write('<!DOCTYPE html>')
+
+with open('food_info.html', 'a') as f:
+    f.write('<html>')
+    f.write(f'<title>{actual_meal}</title>')
+    f.write(f'<h1>{actual_meal}</h1>')
+    f.write(f'<img src="food.jpg">')
+    f.write(f'<p>{recipe}</p>')
+    f.write('</html>')
--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-site = requests.get('https://www.themealdb.com/api/json/v1/1/categories.php').text
-print(site)
